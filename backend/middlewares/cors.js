b67d71461ab2dd26702a87dfd4afec891f580614
Anton Geras <jutsio@yandex.ru>
2023-05-14 01:57:56 +0300

    Изменить мидлвару cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,14 +1,10 @@
 const cors = (req, res, next) => {
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['Access-Control-Request-Headers'];
-  console.log(requestHeaders);
-  console.log(req.header);
-  console.log(req.headers);
   res.header('Access-Control-Allow-Origin', '*');
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Headers', 'content-type');
   }
   next();
 };
