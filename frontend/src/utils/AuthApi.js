ded9d889aa9d14986296cfc31076257fc182e1db
Anton Geras <jutsio@yandex.ru>
2023-05-13 23:38:22 +0300

    Добавить https в запросах

--- frontend/src/utils/AuthApi.js
+++ frontend/src/utils/AuthApi.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://84.252.128.243:3000';
+export const BASE_URL = 'https://84.252.128.243';
 
 export const register = (password, email) => {
     return fetch(`${BASE_URL}/signup`, {
