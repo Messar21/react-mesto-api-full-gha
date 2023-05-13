b06dbce161ed706144782890004d92cc797ecb19
Anton Geras <jutsio@yandex.ru>
2023-05-14 01:25:27 +0300

    Изменить мидлвару cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,12 +1,21 @@
 const cors = (req, res, next) => {
+  const allowedCors = [
+    'https://praktikum.tk',
+    'http://praktikum.tk',
+    'localhost:3000',
+  ];
+  const { origin } = req.headers;
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['Access-Control-Request-Headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
   }
-  res.header('Access-Control-Allow-Origin', '*');
   next();
 };
 
